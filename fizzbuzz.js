b2a2c0d3da4d60200c791d6c03d3aa5fed9f26e0
MCC Local Administrator <em_ennis@outlook.com>
2018-10-04 20:26:58 -0400

    First commit

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+let num = 1;
+
+while (num <= 100) {
+    console.log(num);
+    num = num+=1;
+if (num % 3 === 0) {
+    console.log("Fizz");
+}
+else if (num % 5 === 0) {
+    console.log("Buzz");
+    }
+}
---
+++ triangle.js
@@ -0,0 +1,6 @@
+let hash = "#";
+while (hash.length <= 7) {
+    console.log(hash);
+    hash = hash+="#";
+
+}
